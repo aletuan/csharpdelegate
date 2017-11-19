8a6f2934fb184e2778539a61d64838ef6a28ffe8
Tuan Anh Le <anhlt1983@gmail.com>
2017-11-19 21:49:41 +0900

    Invocation list with delegate instance

--- Program.cs
+++ Program.cs
@@ -6,26 +6,43 @@ namespace csharpdelegate
     {
         // define a public delegate
         public delegate int Calculate(int x, int y);
+        public delegate void Del();
 
         // define some method that can be invoke by delegate instance
         public int Add(int x, int y) 
         {
+            Console.WriteLine("Add");
             return x + y;
         }
 
         public int Multiply(int x, int y)
         {
+            Console.WriteLine("Multiple");
             return x * y;
+        }
+
+        public void MethodOne() 
+        {
+            Console.WriteLine("MethodOne");
+        }
+
+        public void MethodTwo()
+        {
+            Console.WriteLine("MethodTwo");
+        }
+
+        public void Multicast()
+        {
+            Del d = MethodOne;
+            d += MethodTwo;
+            Console.WriteLine("Get invocation list {0}", d.GetInvocationList().GetLength(0));
+            d();
         }
 
         static void Main(string[] args)
         {
             Program main = new Program();
-            Calculate calc = main.Add;
-            Console.WriteLine("Add result {0}", calc(10, 10));
-            
-            calc = main.Multiply;
-            Console.WriteLine("Multiple result {0}", calc(10, 10));
+            main.Multicast();
         }
     }
 }
