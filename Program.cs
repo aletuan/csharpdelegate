e3994a0e5d3d24446559c53ce277fbb86dc76d02
Tuan Anh Le <anhlt1983@gmail.com>
2017-11-19 21:24:26 +0900

    Initial console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharpdelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
