a2ed3db17d7856fd86bea9a54293b42720778903
Tuan Anh Le <anhlt1983@gmail.com>
2017-11-19 21:30:42 +0900

    using delegate to invoke method

--- Program.cs
+++ Program.cs
@@ -4,9 +4,28 @@ namespace csharpdelegate
 {
     class Program
     {
+        // define a public delegate
+        public delegate int Calculate(int x, int y);
+
+        // define some method that can be invoke by delegate instance
+        public int Add(int x, int y) 
+        {
+            return x + y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program main = new Program();
+            Calculate calc = main.Add;
+            Console.WriteLine("Add result {0}", calc(10, 10));
+            
+            calc = main.Multiply;
+            Console.WriteLine("Multiple result {0}", calc(10, 10));
         }
     }
 }
